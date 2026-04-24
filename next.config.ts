33cc4097751228bedbd41971c91a1f1ba7617643
Pierre Baijot <pierre.baijot@gmail.com>
2026-04-24 16:55:09 +0200

    fix: remove unsupported eslint option from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 export default withNextIntl({
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       { hostname: 'cdn.sanity.io' },
