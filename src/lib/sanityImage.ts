c367c650886af3ab504902713ef7ce7ca5a066e3
Tayeb <tayeb.hussain02@gmail.com>
2026-04-28 17:11:43 +0200

    fix: sanityImage SanityImageSource type

--- src/lib/sanityImage.ts
+++ src/lib/sanityImage.ts
@@ -1,9 +1,10 @@
 import imageUrlBuilder from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import {sanity} from './sanity'
 
 const builder = sanity ? imageUrlBuilder(sanity) : null
 
-export function urlFor(source: unknown) {
+export function urlFor(source: SanityImageSource) {
   if (!builder) return null
   return builder.image(source)
 }
