64bbbd84545a379275ab767501618f6fb8dff66a
Tayeb <tayeb.hussain02@gmail.com>
2026-04-30 14:46:31 +0200

    fix: neutralise simulateur + layoutConstants

--- app/[locale]/components/HomeTunnel.tsx
+++ app/[locale]/components/HomeTunnel.tsx
@@ -1,13 +1,10 @@
 'use client'
 import { useLocale } from 'next-intl'
-import Simulator from '@/simulator/src/Simulator'
 
 export default function HomeTunnel() {
-  const locale = useLocale()
   return (
-    <Simulator
-      devisUrl={'/' + locale + '/devis'}
-      soumissionUrl={'/' + locale + '/pro/soumission'}
-    />
+    <div className="w-full h-full flex items-center justify-center bg-black">
+      <div className="text-white/30 text-sm">Simulateur bientôt disponible</div>
+    </div>
   )
 }
---
+++ simulator/src/components/layout/layoutConstants.ts
@@ -0,0 +1,2 @@
+export const RIGHT_PANEL_WIDTH_PX = 320
+export const RIGHT_PANEL_OFFSET_PX = 0
